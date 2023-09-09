e8e9e6693c23e89aaba7a7985b43f66566434ea9
kioharry0 <kio.harry1@gmail.com>
2023-09-09 00:03:16 -0700

    for

---
+++ 5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program that prints the alphabet in lowercase and upper case
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d\n", i);
+}
+return (0);
+}
