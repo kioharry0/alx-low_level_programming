378b0972313f75ec393c2665806ffc2401a0b6cf
kioharry0 <kio.harry1@gmail.com>
2023-09-19 17:14:25 -0700

    pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98 - the function name 
- * @n: function parameter
- */
-
+* reset_to_98 - the function name
+* @n: function parameter
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
-
 }
-
